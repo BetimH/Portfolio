cd9ecd0739d6843309cbb72032b7aba6731b58d2
web <103862044+BetimH@users.noreply.github.com>
2025-05-12 01:39:58 +0200

    Update index.js/animations

--- index.js
+++ index.js
@@ -15,3 +15,80 @@ function closeNav() {
     document.getElementsByClassName("main")[0].style.backgroundColor = "rgba(0,0,0,0.5)";
   
 }
+
+
+
+
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.text');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 100) { // Adjust scroll threshold as needed
+        target.classList.add('scroll-fade-out');
+    } else {
+        target.classList.remove('scroll-fade-out');
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.main');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 100) { // Adjust scroll threshold as needed
+        target.classList.add('scroll-fade-out');
+    } else {
+        target.classList.remove('scroll-fade-out');
+    }
+});
+
+
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.images');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 200) { // Adjust scroll threshold as needed
+        target.classList.add('slideUp');
+    } else {
+        target.classList.remove('slideUp');
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.text2');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 200) { // Adjust scroll threshold as needed
+        target.classList.add('slideUp');
+    } else {
+        target.classList.remove('slideUp');
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.text1');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 200) { // Adjust scroll threshold as needed
+        target.classList.add('slideUp');
+    } else {
+        target.classList.remove('slideUp');
+    }
+});
+
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.main3');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 300) { // Adjust scroll threshold as needed
+        target.classList.add('slideUp');
+    } else {
+        target.classList.remove('slideUp');
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const target = document.querySelector('.main4');
+     // Replace '.text' with the element you want to fade out
+    if (window.scrollY > 500) { // Adjust scroll threshold as needed
+        target.classList.add('fadeIn');
+    } else {
+        target.classList.remove('fadeIn');
+    }
+});
